0d7a3570bff6542d0f70a42519fac74382b0b4d4
ShamReh <sr.166@hotmail.co.uk>
2021-11-01 17:41:25 +0000

    Learning TS

---
+++ basics.ts
@@ -0,0 +1,20 @@
+// assigning types to data types not directly declared
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+  //   throw new Error('Incorrect Input');
+  // } runtime check in JavaScript not needed for TS
+  const result= n1+n2
+    if (showResult) {
+    console.log(phrase+result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase= "Result is: "
+
+add(number1, number2, printResult, resultPhrase);
+
